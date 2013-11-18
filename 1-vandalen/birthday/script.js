d7f9606f43e0b2481d125b7be3a8d6ccd131eca5
Emil Dannberger <dannberger3090@hotmail.com>
2013-11-18 14:21:30 +0100

    Labb 1.3 Klar.

--- 1-vandalen/birthday/script.js
+++ 1-vandalen/birthday/script.js
@@ -7,9 +7,27 @@ window.onload = function(){
 
         // Din kod här.
 
+        // Kontrollerar ifall rätt format på datumet används.
+        var IsoDateRe = new RegExp("^([0-9]{4})-([0-9]{2})-([0-9]{2})");
+        var matches = IsoDateRe.exec(date);
+        if (!matches) {
+            throw new Error("FEL! Ange ditt födelsedatum i formatet ÅÅÅÅ-MM-DD.");
+        }
 
+        //Skapar Date-objekt för jämförelse av födelsedar och dagens datum.
 
+        var dateParts = date.split("-");
 
+        var now = new Date()
+        var currentYear = now.getFullYear()
+        var dateEnd = new Date(currentYear, dateParts[1] - 1, dateParts[2]); // -1 på månaderna eftersom de har talbasen 0.
+
+        // Har datumet redan passerats så läggs det på 1 på nuvarande året. Detta gör att det skrivs ut heltal istället för negativa tal.
+        if (dateEnd - now < 0) {
+            dateEnd.setFullYear(currentYear + 1);
+        }
+
+        return Math.ceil((dateEnd - now) / 1000 / 60 / 60 / 24);
 
 	};
 	// ------------------------------------------------------------------------------
