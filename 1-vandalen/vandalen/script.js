09673f68d5fdaddaa4f46ea015dde472fcd4d68d
Emil Dannberger <dannberger3090@hotmail.com>
2013-11-18 17:42:30 +0100

    1.4 startad.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,30 @@
 "use strict";
 
-var makePerson = function(persArr){
+var makePerson = function (persArr) {
+
+    // Skapar en array med enbart de medskickade namnen.
+    for (var i = 0; i < persArr.length; i++) {
+        var names = persArr[i].name;
+    }
 
 
-	// Din kod här...
+    // Funktion som sorterar namnen i alfabetisk ordning.
+    var sortedNames = persArr.sort(function (a, b) {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+    });
 
+    console.log(sortedNames[0],sortedNames[1],sortedNames[2]); // kollar så att namnen är sorterade.
+
+    // Skapar ett objekt med värdena som ska skickas tillbaks.
+    var obj = {
+        minAge: 0,
+        maxAge: 0,
+        averageAge: 0,
+        names: ''
+    };
+
+    return obj;
 }
 
