bd1cbd2e9100fe4cf3b5d388d334f049ce492bc9
Emil Dannberger <dannberger3090@hotmail.com>
2013-11-19 10:29:32 +0100

    Labb 1.4 forts.

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,27 +2,24 @@
 
 var makePerson = function (persArr) {
 
-    // Skapar en array med enbart de medskickade namnen.
-    for (var i = 0; i < persArr.length; i++) {
-        var names = persArr[i].name;
-    }
-
-
     // Funktion som sorterar namnen i alfabetisk ordning.
-    var sortedNames = persArr.sort(function (a, b) {
+    persArr.sort(function (a, b) {
         if (a.name < b.name) return -1;
         if (a.name > b.name) return 1;
         return 0;
     });
 
-    console.log(sortedNames[0],sortedNames[1],sortedNames[2]); // kollar så att namnen är sorterade.
+    var str = persArr.map(function (pers) {
+        return pers.name;
+    }).join(", ");
+
 
     // Skapar ett objekt med värdena som ska skickas tillbaks.
     var obj = {
         minAge: 0,
         maxAge: 0,
         averageAge: 0,
-        names: ''
+        names: str
     };
 
     return obj;
