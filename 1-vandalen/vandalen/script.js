dcbd6e1b26e5f9859a9055709500a0b9d7ab93eb
Emil Dannberger <dannberger3090@hotmail.com>
2013-11-19 19:56:21 +0100

    Labb 1 klar!

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -2,23 +2,42 @@
 
 var makePerson = function (persArr) {
 
+    // Tar fram maxvärdet av åldrarna i arrayen.
+    var max = Math.max.apply(null,
+                            Object.keys(persArr).map(function (e) {
+                                return persArr[e]['age'];
+                            }));
+
+    // Tar fram minvärdet av åldrarna i arrayen.
+    var min = Math.min.apply(null,
+                            Object.keys(persArr).map(function (e) {
+                                return persArr[e]['age'];
+                            }));
+
+    // Tar fram medelvärdet av åldrarna i arrayen.
+    var sum = 0;
+
+    for (var i = 0; i < persArr.length; i++) {
+        sum += parseInt(persArr[i].age);
+    }
+
+    var avg = Math.round(sum / persArr.length);
+
     // Funktion som sorterar namnen i alfabetisk ordning.
     persArr.sort(function (a, b) {
-        if (a.name < b.name) return -1;
-        if (a.name > b.name) return 1;
-        return 0;
+        return a.name.localeCompare(b.name)
     });
 
+    // Skapar en sträng utav de sorterade namnen.
     var str = persArr.map(function (pers) {
         return pers.name;
     }).join(", ");
 
-
     // Skapar ett objekt med värdena som ska skickas tillbaks.
     var obj = {
-        minAge: 0,
-        maxAge: 0,
-        averageAge: 0,
+        minAge: min,
+        maxAge: max,
+        averageAge: avg,
         names: str
     };
 
