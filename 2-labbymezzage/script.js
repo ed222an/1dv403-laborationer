60fa766a2880eb6d5329cac85f717f3db430c953
Emil Dannberger <dannberger3090@hotmail.com>
2013-11-27 14:19:23 +0100

    Labb 2, steg 2 klart.

--- 2-labbymezzage/script.js
+++ 2-labbymezzage/script.js
@@ -2,7 +2,12 @@
 
 window.onload = function init() {
 
-    
+    var mess = new Message("Testmeddelande", new Date());
+    alert(mess);
+    alert(mess.getText(), mess.getDate);
+    mess.setText("En annan text");
+    mess.setDate(Date());
+    alert(mess);
 
 
 
