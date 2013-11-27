b0b642d65e766fe1a27b7f8db36760835855c391
Emil Dannberger <dannberger3090@hotmail.com>
2013-11-27 15:19:46 +0100

    Labb 2, steg 3 klart.

--- 2-labbymezzage/MessageBoard.js
+++ 2-labbymezzage/MessageBoard.js
@@ -1,17 +1,34 @@
 "use strict";
 
-window.onload = function init() {
+var MessageBoard = {
 
-    var mess = new Message("Testmeddelande", new Date());
-    alert(mess);
-    alert(mess.getText(), mess.getDate);
-    mess.setText("En annan text");
-    mess.setDate(Date());
-    alert(mess);
+    messages: [],
+
+    init: function (e) {
+        
+        //Testar message-objektet.
+        //var mess = new Message("Testmeddelande", new Date());
+        //alert(mess);
+        //alert(mess.getText);
+        //mess.setText("En annan text");
+        //alert(mess);
 
 
+        //Testar message-arrayen.
+        //var test1 = new Message("Test 1", new Date());
+        //var test2 = new Message("Test 2", new Date());
+        //var test3 = new Message("Test 3", new Date());
+        //var test4 = new Message("Test 4", new Date());
+
+        //MessageBoard.messages.push(test1, test2, test3, test4);
+
+        //console.log(MessageBoard.messages[0].getText());
+        //console.log(MessageBoard.messages[1].getText());
+        //console.log(MessageBoard.messages[2].getText());
+        //console.log(MessageBoard.messages[3].getText());
 
 
-
-
+    }
 };
+
+window.onload = MessageBoard.init;
