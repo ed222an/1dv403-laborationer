97253a8b0e0fa26b9ac47e30f1b094b1c9104b2d
Emil Dannberger <dannberger3090@hotmail.com>
2013-12-10 11:31:18 +0100

    Labb 3 forts.

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -11,7 +11,6 @@ var Memory = {
         Memory.saveArray(RandomGenerator.getPictureArray(4, 4));
 
         Memory.renderArray(memoryArray);
-
     },
 
     saveArray: function (randomGeneratedArray) {
@@ -28,12 +27,8 @@ var Memory = {
         newDiv.id = "block";
         newDiv.className = "block";
         document.getElementsByTagName("body")[0].appendChild(newDiv);
-
-        // Skapar en inre div-tag
         var innerDiv = document.createElement("div");
         innerDiv.className = "block-2";
-
-        // Lägger till den inre divtaggen i den yttre.
         newDiv.appendChild(innerDiv);
 
         // Skapar en tabell och placerar den i HTML-dokumentet
@@ -45,24 +40,55 @@ var Memory = {
             var row = document.createElement("tr");
             table.appendChild(row);
 
-            // Lägger till respektive siffra i arrayen i en tabellcell.
-            for (var j = 0; j < myArray.length; ++j) {
-                var arrayNumber = document.createTextNode(myArray[j]);
+            // Skapar en cell med respektive siffra.
+            for (var j = 0; j < 4; ++j) {
+                var cell = document.createElement("td");
 
-                // Skapar en cell med respektive siffra.
-                for (var k = 0; k < 4; ++k) {
-                    var cell = document.createElement("td");
-                    row.appendChild(cell);
-                    cell.appendChild(arrayNumber);
+                // Översätter arrayens siffror till bilder & lägger in dem i a-taggar.
+                var image = document.createElement("img");
+                image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/0.png?raw=true";
+                var aTag = document.createElement("a");
+                aTag.onclick = function () {
+
                 };
+
+                // Lägger till respektive bild i en tabellcell.
+                aTag.appendChild(document.createTextNode(myArray[i * 4 + j]));
+                aTag.appendChild(image);
+                cell.appendChild(aTag);
+                row.appendChild(cell);
             };
         };
-
         innerDiv.appendChild(table);
+    },
 
+    flipTile: function (arrayValue) {
 
-
-
+        // Vänder brickorna.
+        if (arrayValue === 1) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/1.png?raw=true";
+        };
+        if (arrayValue === 2) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/2.png?raw=true";
+        };
+        if (arrayValue === 3) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/3.png?raw=true";
+        };
+        if (arrayValue === 4) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/4.png?raw=true";
+        };
+        if (arrayValue === 5) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/5.png?raw=true";
+        };
+        if (arrayValue === 6) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/6.png?raw=true";
+        };
+        if (arrayValue === 7) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/7.png?raw=true";
+        };
+        if (arrayValue === 8) {
+            image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/8.png?raw=true";
+        };
     }
 
 };
@@ -73,3 +99,28 @@ window.onload = Memory.init;
 //for (var i = 0; i < memoryArray.length; ++i) {
 //    console.log(memoryArray[i]);
 //}
+
+//if (myArray[i * 4 + j] === 1) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/1.png?raw=true";
+//};
+//if (myArray[i * 4 + j] === 2) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/2.png?raw=true";
+//};
+//if (myArray[i * 4 + j] === 3) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/3.png?raw=true";
+//};
+//if (myArray[i * 4 + j] === 4) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/4.png?raw=true";
+//};
+//if (myArray[i * 4 + j] === 5) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/5.png?raw=true";
+//};
+//if (myArray[i * 4 + j] === 6) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/6.png?raw=true";
+//};
+//if (myArray[i * 4 + j] === 7) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/7.png?raw=true";
+//};
+//if (myArray[i * 4 + j] === 8) {
+//    image.src = "https://github.com/1dv403/1dv403-laborationer/blob/master/3-gameon/memory/pics/8.png?raw=true";
+//};
