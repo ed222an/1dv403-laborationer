2cd1a99671bfd5ed3a765452c58ee0147d47522c
Emil Dannberger <dannberger3090@hotmail.com>
2013-12-17 10:33:21 +0100

    Labb 4, jobbat med textrutorna.

--- 4-forms/validator.js
+++ 4-forms/validator.js
@@ -13,39 +13,69 @@ var Validator = {
 
         firstName.onblur = function () {
 
-            // Variabel för testning av tom sträng.
+            // Variabel för testning av strängen.
             var fnValue = firstName.value;
-
             fnValue = fnValue.replace(/^\s+/, '').replace(/\s+$/, '');
+
+            //Är strängen tom eller whitespace körs errorBox. Annars körs correctBox.
             if (fnValue === '') {
-                // Texten var bara whitespace.
-                console.log("Firstname = whitespace");
+                Validator.errorBox(firstName);
+            }
+            else {
+                Validator.correctBox(firstName);
             };
         };
 
         lastName.onblur = function () {
 
-            // Variabel för testning av tom sträng.
+            // Variabel för testning av strängen.
             var lnValue = lastName.value;
-
             lnValue = lnValue.replace(/^\s+/, '').replace(/\s+$/, '');
+
+            //Är strängen tom eller whitespace körs errorBox. Annars körs correctBox.
             if (lnValue === '') {
-                // Texten var bara whitespace.
-                console.log("Lastname = whitespace");
+                Validator.errorBox(lastName);
+            }
+            else {
+                Validator.correctBox(lastName);
             };
         };
 
         postalCode.onblur = function () { // LÄGG TILL CHECK FÖR NUMMER!!!!
 
-            // Variabel för testning av tom sträng.
+            // Variabel för testning av strängen.
             var pcValue = postalCode.value;
-
             pcValue = pcValue.replace(/^\s+/, '').replace(/\s+$/, '');
+
+            //Är strängen tom eller whitespace körs errorBox. Annars körs correctBox.
             if (pcValue === '') {
-                // Texten var bara whitespace.
-                console.log("Postalcode = whitespace");
+                Validator.errorBox(postalCode);
+            }
+            else {
+                Validator.correctBox(postalCode);
             };
         };
+    },
+
+    errorBox: function (inputBox) {
+
+        // Gör rutan röd.
+        inputBox.className = "red";
+
+        var text = document.createTextNode("This box must be filled out!");
+
+        if (inputBox.id == "firstName") {
+            console.log("test");
+            var p = document.getElementById("pFirst");
+            p.appendChild(text);
+        };
+
+    },
+
+    correctBox: function (inputBox) {
+
+        // Gör rutan grön.
+        inputBox.className = "green";
     }
 
 };
